ff3b24d7478e5bd876be813a635b07372798deb7
Corentin Labbe <clabbe.montjoie@gmail.com>
2019-03-08 16:15:37 +0100

    scripts/gentoo_add_sources.py: more debug and handle 5011-xxx-patch

--- scripts/gentoo_add_sources.py
+++ scripts/gentoo_add_sources.py
@@ -5,6 +5,8 @@ import os
 import subprocess
 import argparse
 
+# TODO gpg and other checks
+
 gentoo_source_dir = "/usr/src/gentoo/"
 cachedir = "%s/cache/" % os.getcwd()
 
@@ -15,6 +17,7 @@ os.environ["LANG"] = "C"
 parser = argparse.ArgumentParser()
 parser.add_argument("--ltag", type=str, help="Select device via some tags")
 parser.add_argument("--sourcedir", type=str, help="where to store sources")
+parser.add_argument("--debug", "-d", type=bool, help="where to store sources")
 args = parser.parse_args()
 
 
@@ -35,16 +38,19 @@ else:
     finaldir = args.sourcedir
     print("DEBUG: will handle %s" % finaldir)
 
-tags = subprocess.check_output("cd %s/linux-patches && git branch -a | grep /[0-9] | sort -V | tail -n2 |sed 's,..*/,,'" % cachedir, shell=True)
+tags = subprocess.check_output("cd %s/linux-patches && git branch -a | grep /[0-9] | sort -V | tail -n10 |sed 's,..*/,,'" % cachedir, shell=True)
 for tag in tags.decode("UTF-8").split('\n'):
     if tag == "":
         continue
     if args.ltag is not None and args.ltag != tag:
+        print("DEBUG: ignore %s" % tag)
         continue
-    print("Handle %s" % tag)
+    print("Handle tag %s" % tag)
     try:
-        subprocess.check_output("cd %s && wget -N https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-%s.tar.xz" % (cachedir, tag), shell = True)
+        major = tag.split(".")[0]
+        subprocess.check_output("cd %s && wget -N https://cdn.kernel.org/pub/linux/kernel/v%s.x/linux-%s.tar.xz" % (cachedir, major, tag), shell = True)
     except:
+        print("ERROR: fail to download %s" % tag)
         continue
     sourcename = "gentoo-%s" % tag
     subprocess.check_output("cd %s/linux-patches && git checkout %s" % (cachedir, tag), shell = True)
@@ -52,20 +58,29 @@ for tag in tags.decode("UTF-8").split('\n'):
     if finaldir == None:
         finaldir = "%s/linux-%s" % (gentoo_source_dir, tag)
     if os.path.isdir(finaldir):
-        floc = open("%s.commit" % finaldir, 'r')
-        lastcommit = floc.read()
-        floc.close()
+        if os.path.isfile("%s.commit" % finaldir):
+            floc = open("%s.commit" % finaldir, 'r')
+            lastcommit = floc.read()
+            floc.close()
+        else:
+            lastcommit = "notfound"
         print("Already extracted %s %s" % (git_lastcommit, lastcommit))
         if (lastcommit == git_lastcommit):
             print("no change")
             continue
+        if args.debug:
+            print("DEBUG: remove old source")
         subprocess.check_output("rm -r %s" % finaldir, shell = True)
     flc = open("%s.commit" % finaldir, 'w')
     flc.write(git_lastcommit)
     flc.close()
     if not os.path.isdir(finaldir):
         os.mkdir(finaldir)
+    if args.debug:
+        print("DEBUG: Extract sources")
     subprocess.check_output("tar xJf %s/linux-%s.tar.xz -C %s/.." % (cachedir, tag, finaldir), shell = True)
+    if args.debug:
+        print("DEBUG: Checkout latest %s" % tag)
     subprocess.check_output("cd %s/linux-patches && git checkout %s" % (cachedir, tag), shell = True)
     print("Apply patches")
     patchs = subprocess.check_output("cd %s && ls %s/linux-patches/*patch" % (finaldir, cachedir), shell = True)
@@ -74,4 +89,7 @@ for tag in tags.decode("UTF-8").split('\n'):
         if patch == "":
             continue
         print("Apply %s\n" % patch)
-        subprocess.check_output("cd %s && patch -p1 < %s" % (finaldir, patch), shell = True)
+        if patch == "/home/compile/bbci/cache//linux-patches/5011_enable-cpu-optimizations-for-gcc8.patch":
+            continue
+        ret = subprocess.check_output("cd %s && patch -p1 < %s" % (finaldir, patch), shell = True)
+        print(ret)
